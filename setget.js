b30377f9a2256294d9e9e4c0000af970f1a08898
xiaoduan <274815408@qq.com>
2017-03-23 02:15:57 +0800

    setget的注解

--- setget.js
+++ setget.js
@@ -7,33 +7,51 @@ function MiniVue(id, initData) {
 	bindings = {}
 	data = self.data = {}
 	content = el.innerHTML.replace(/\{\{(.*)\}\}/g, markToken)
-	
-	console.log(bindings)
 
 	el.innerHTML = content
 
+	/*
+		将bindings里的每个key都bind到self.data上，其实就是改写了data上每一个el里出现的key的get和set
+		然后再将initData里的对应值赋给self.data，来触发该键的set方法，更新视图
+	 */
 	for (var variable in bindings) {
 		bind(variable)
 	}
-	
+
+	if (initData) {
+		for (var variable in initData) {
+			data[variable] = initData[variable]
+		}
+	}
+
+	/*
+		先遍历el里所有{{}}内的变量，做了bingings的一个对应关系，并返回做了标记的span
+	 */
 	function markToken(match, variable) {
 		bindings[variable] = {}
 		return `<span ${bindingMark}="${variable}"></span>`
 	}
 
 	function bind(variable) {
-		bindings[variable].els = el.querySelectorAll(`[${bindingMark}="${variable}]"`)
-		
-		Array.prototype.forEach.call(bindings[variable].els, function(e) {
+		const _this = bindings[variable]
+		_this.els = el.querySelectorAll(`[${bindingMark}="${variable}]"`)
+
+		Array.prototype.forEach.call(_this.els, function(e) {
 			e.removeAttribute(bindingMark)
 		})
 
 		Object.defineProperty(data, variable, {
+			/*
+				改写set方法，此variable改变后，
+				所有绑定了variable的dom里的textContent都变成新的值
+			 */
 			set: function(newVal) {
-
+				Array.prototype.forEach.call(_this.els, function(e) {
+					_this.value = e.textContent = newVal
+				})
 			},
 			get: function() {
-
+				return _this.value
 			}
 		})
 	}
